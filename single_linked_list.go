784ccd55a2c122cb7a91b2a7d5ad2640e755ee8b
adityarizkyramadhan <90701908+adityarizkyramadhan@users.noreply.github.com>
2022-05-19 09:24:26 +0700

    Bug Fixed

--- single_linked_list.go
+++ single_linked_list.go
@@ -2,71 +2,65 @@ package data_structure_go
 
 import "fmt"
 
-type nodeSLL struct {
+type nodeSll struct {
 	Data interface{}
-	Next *nodeSLL
+	Next *nodeSll
 }
 
-type sLList struct {
-	Head   *nodeSLL
-	length int
+type singleLinkedList struct {
+	Head *nodeSll
 }
 
-func NewSingleLinkedList() *sLList {
-	return &sLList{}
+func NewSingleLinkedList() *singleLinkedList {
+	return &singleLinkedList{}
 }
 
-func (n *sLList) AddDataLast(data interface{}) {
-	nextNode := nodeSLL{Data: data}
+func (n *singleLinkedList) AddDataLast(data interface{}) {
+	nextNode := nodeSll{Data: data}
 	if n.Head == nil {
 		n.Head = &nextNode
 		return
 	}
-	currentNodeSLL := n.Head
-	for currentNodeSLL != nil {
-		currentNodeSLL = currentNodeSLL.Next
-		//add data to last
-		if currentNodeSLL.Next == nil {
-			currentNodeSLL.Next = &nextNode
-			n.length++
-			break
-		}
+	currNode := n.Head
+	for currNode.Next != nil {
+		currNode = currNode.Next
 	}
+	currNode.Next = &nextNode
 }
 
-func (n *sLList) AddDataFirst(data interface{}) {
-	newNodeSLL := &nodeSLL{Data: data}
+func (n *singleLinkedList) AddDataFirst(data interface{}) {
+	newnodeSll := &nodeSll{Data: data}
 	if n.Head != nil {
-		newNodeSLL.Next = n.Head
-		n.Head = newNodeSLL
-		n.length++
+		newnodeSll.Next = n.Head
+		n.Head = newnodeSll
+
 	} else {
 		temp := n.Head
-		newNodeSLL.Next = temp
-		n.Head = newNodeSLL
+		newnodeSll.Next = temp
+		n.Head = newnodeSll
+
 	}
 }
 
-func (n *sLList) InsertDataAfter(index int, data interface{}) {
+func (n *singleLinkedList) InsertDataAfter(index int, data interface{}) {
 	if n.Head == nil {
 		fmt.Println("Error, Null LinkedList")
 		return
 	}
-	newNodeSLL := &nodeSLL{Data: data}
-	currentNodeSLL := n.Head
+	newnodeSll := &nodeSll{Data: data}
+	currentnodeSll := n.Head
 	for i := 0; i < index; i++ {
-		if currentNodeSLL.Next == nil {
+		if currentnodeSll.Next == nil {
 			fmt.Println("Error, Index out of range")
 			return
 		}
-		currentNodeSLL = currentNodeSLL.Next
+		currentnodeSll = currentnodeSll.Next
 	}
-	newNodeSLL.Next = currentNodeSLL.Next
-	currentNodeSLL.Next = newNodeSLL
-	n.length++
+	newnodeSll.Next = currentnodeSll.Next
+	currentnodeSll.Next = newnodeSll
 }
 
-func (n *sLList) InsertDataBefore(index int, data interface{}) {
+func (n *singleLinkedList) InsertDataBefore(index int, data interface{}) {
 	if index == 0 {
 		fmt.Println("Error, you can't insert data before zero")
 		return
@@ -75,7 +69,7 @@ func (n *sLList) InsertDataBefore(index int, data interface{}) {
 		fmt.Println("Error, Null LinkedList")
 		return
 	}
-	newNode := &nodeSLL{Data: data}
+	newNode := &nodeSll{Data: data}
 	currentNode := n.Head
 	for i := 0; i < index-1; i++ {
 		if currentNode.Next == nil {
@@ -85,34 +79,32 @@ func (n *sLList) InsertDataBefore(index int, data interface{}) {
 		currentNode = currentNode.Next
 	}
 	newNode.Next = currentNode.Next
-	currentNode.Next = newNode
-
+	currentNode.Next = newNode.Next
 }
 
 //Not used because data type is interface{} so i don't give method print in this case
-// func (n *sLList) Print() {
+// func (n *singleLinkedList) Print() {
 // 	if n.Head == nil {
 // 		return
 // 	}
-// 	currentNodeSLL := n.Head
-// 	for currentNodeSLL != nil {
-// 		fmt.Println(currentNodeSLL.Data)
-// 		currentNodeSLL = currentNodeSLL.Next
+// 	currentnodeSll := n.Head
+// 	for currentnodeSll != nil {
+// 		fmt.Println(currentnodeSll.Data)
+// 		currentnodeSll = currentnodeSll.Next
 // 	}
 // 	fmt.Println()
 // }
 
-func (n *sLList) RemoveDataByIndex(index int) interface{} {
+func (n *singleLinkedList) RemoveDataByIndex(index int) interface{} {
 	if n.Head == nil {
 		fmt.Println("Empty linked list")
 		return nil
 	}
-	var temp *nodeSLL
+	var temp *nodeSll
 	currNode := n.Head
 	if index == 0 {
 		temp = n.Head
 		n.Head = nil
-		n.length--
 		return temp.Data
 	}
 	for i := 0; i < index-1; i++ {
@@ -124,18 +116,17 @@ func (n *sLList) RemoveDataByIndex(index int) interface{} {
 	}
 	temp = currNode.Next
 	currNode.Next = currNode.Next.Next
-	n.length--
 	return temp.Data
 }
 
-func (n *sLList) GetDataByIndex(index int) interface{} {
+func (n *singleLinkedList) GetDataByIndex(index int) interface{} {
 	if n.Head == nil {
 		fmt.Println("Empty linked list")
 		return nil
 	}
 	currNode := n.Head
 	if index == 0 {
-		return currNode.Data
+		return n.Head.Data
 	}
 	for i := 0; i < index; i++ {
 		if currNode.Next == nil {
@@ -147,10 +138,17 @@ func (n *sLList) GetDataByIndex(index int) interface{} {
 	return currNode.Data
 }
 
-func (n *sLList) Len() int {
-	return n.length
+func (n *singleLinkedList) Len() int {
+	// TODO: count elements in linked list
+	curr := n.Head
+	count := 0
+	for curr != nil {
+		count++
+		curr = curr.Next
+	}
+	return count
 }
 
-func (n *sLList) IsEmpty() bool {
-	return n.Head == nil
+func (n *singleLinkedList) IsEmpty() bool {
+	return n.Len() == 0
 }
--- sll_test.go
+++ sll_test.go
@@ -15,5 +15,13 @@ func TestSLL(t *testing.T) {
 	sll.AddDataLast(mahasiswa{Name: "Aditya", Grade: 99})
 	data := sll.RemoveDataByIndex(0)
 	fmt.Println(data.(mahasiswa).Name)
-	fmt.Println(sll.IsEmpty())
+	// fmt.Println(sll.IsEmpty())
+	sll.AddDataLast(mahasiswa{Name: "Aditya", Grade: 99})
+	sll.AddDataLast(mahasiswa{Name: "Rizky", Grade: 99})
+	sll.AddDataLast(mahasiswa{Name: "Ramadhan", Grade: 99})
+	fmt.Println(sll.Len())
+	fmt.Println(sll.GetDataByIndex(1).(mahasiswa).Name)
+	for i := 0; i < sll.Len(); i++ {
+		fmt.Println(sll.GetDataByIndex(i).(mahasiswa).Name)
+	}
 }
